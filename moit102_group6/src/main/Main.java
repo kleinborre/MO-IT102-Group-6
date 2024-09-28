39db6ccf6fe040ea19d122274b788a3fbc083a42
kleinborre <131511662+kleinborre@users.noreply.github.com>
2024-09-28 10:52:57 +0800

    added csv folder and csv file of motorph inventory data

--- moit102_group6/src/main/Main.java
+++ moit102_group6/src/main/Main.java
@@ -4,33 +4,5 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// create an array
-		int[] age = {1,2,3,4,5};
-		
-		// accessing each element inside the array
-		
-		System.out.println("The element is: " + age[0]);
-		System.out.println("The element is: " + age[1]);
-		System.out.println("The element is: " + age[2]);
-		System.out.println("The element is: " + age[3]);
-		System.out.println("The element is: " + age[4]);
-		System.out.println();
-		
-		// or we can do this using for loop
-		
-		for(int num : age) {
-			System.out.println("The element is: " +  num );
-		}
-		
-		// For Array Exercises
-		ArrayExercises ae = new ArrayExercises();
-		// Exercise No.1
-		ae.exercise1();
-		
-		// Exercise No. 2
-		ae.exercise2();
-		
-		//
-		ae.exercise3();
 	}
 }
