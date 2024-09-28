a1e37d1beb4586e8894db42df908c51ec247abbc
kleinborre <131511662+kleinborre@users.noreply.github.com>
2024-09-28 12:16:26 +0800

    Modified some java files

--- moit102_group6/src/main/Main.java
+++ moit102_group6/src/main/Main.java
@@ -4,5 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		// The array below is not the actual MS1 array but for Demonstration purposes only.
+		// This is just to test if the code for algorithms is working properly.
+		// Feel free to change the array after cloning. Enjoy!
+		
+		int arr[] = { 12, 11, 13, 5, 6 };
+		
+		//Compile all the calling methods below;
+		
+		//For Sorting Method
+		Sorting s = new Sorting();
+		s.sort(arr);
+		s.printArray(arr);
+		
+		
 	}
 }
