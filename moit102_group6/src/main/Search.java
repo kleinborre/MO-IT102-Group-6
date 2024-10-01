0e3f279dc56d01470e2da76183a8ccce84730130
kleinborre <131511662+kleinborre@users.noreply.github.com>
2024-10-01 17:10:33 +0800

    Modified code of Search class, added scanner.

--- moit102_group6/src/main/Search.java
+++ moit102_group6/src/main/Search.java
@@ -1,9 +1,18 @@
 package main;
 
+import java.util.Scanner;
+
 public class Search{
 	
 	public static void main(String[] args) {
-        // Inventory data
+        
+		// Call the scanner
+		Scanner scan = new Scanner(System.in);
+		
+		// Store scanner result
+		String target;
+		
+		// Inventory data
         String[][] inventory = {
             {"2/1/2023", "Old", "Honda", "142QVTSIUR", "On-hand"},
             {"2/1/2023", "Old", "Honda", "PZCT1S00XE", "Sold"},
@@ -19,17 +28,27 @@ public class Search{
             {"2/5/2023", "Old", "Kawasaki", "X8G5ZZ7A69", "Sold"}
         };
 
-        // Search for engine number "4VBTV8YNM7"
-        String target = "4VBTV8YNM7";
+        // Search for engine number
+        System.out.print("Search Engine number: ");
+        target = scan.nextLine();
+        
         int result = linearSearch(inventory, target);
 
         // Display result
         if (result != -1) {
+        	System.out.println();
             System.out.println("Engine number found at row " + result);
-            System.out.println("Date Entered: " + inventory[result][0] + ", Brand: " + inventory[result][2]);
+            System.out.println();
+            System.out.println("ENGINE NUMBER " + target + " COMPLETE DETAILS");
+            System.out.println("Date Entered :    " + inventory[result][0]);
+            System.out.println("Stock Label  :    " + inventory[result][1]);
+            System.out.println("Brand        :    " + inventory[result][2]);
+            System.out.println("Status       :    " + inventory[result][4]);
+            
         } else {
             System.out.println("Engine number not found.");
         }
+        scan.close();
     }
 
     // Linear Search Function
@@ -41,5 +60,4 @@ public class Search{
         }
         return -1; // If not found, return -1
     }
-	
 }
