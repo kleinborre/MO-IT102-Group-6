7e4bb8c49292913c58fb82a0b8645bae7a850cf6
kleinborre <131511662+kleinborre@users.noreply.github.com>
2024-11-26 19:58:15 +0800

    Test.

--- moit102_group6/src/main/Main.java
+++ moit102_group6/src/main/Main.java
@@ -3,6 +3,26 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
+		/*
+		 loop 100 times (i)
+		 if i div by 3 & 5: print "FIZZBUZZ"
+		 if i div by 3: print "FIZZ"
+		 if i div by 5: print "BUZZ"
+		 if i div by 7: print "BAZZ"
+		 else: print i
+		 */
+		
+	
+		for(int i = 1; i <= 100; i++) {
+			
+			String result = "";
+			
+			if(i % 3 == 0) result += "Fizz";
+			if(i % 5 == 0) result += "Buzz";
+			if(i % 7 == 0) result += "Bazz";
+			
+			System.out.println(result.isEmpty() ? i : result);
+		}
 		
 	}
 	
